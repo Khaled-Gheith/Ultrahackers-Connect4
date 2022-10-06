dfb3d7af043d15f4d2761b9e408979d9af240d7a
Khaled Gheith <kag07@mail.aub.edu>
2022-10-06 18:13:50 +0300

    Changed the implementation of UpdateBoard function, changed the scope of the board variable to local, passed the board as a parameter to some functions, handled the case where the selected number column is illegal.

--- connect4.c
+++ connect4.c
@@ -6,21 +6,14 @@ char player1[30];
 char player2[30];
 char winner[30];
 int entry;
-double timeTakenByPlayerHavingRed = 0;
-double timeTakenByPlayerHavingYellow = 0;
+long double timeTakenByPlayerHavingRed = 0;
+long double timeTakenByPlayerHavingYellow = 0;
 clock_t start;
 clock_t end;
-int board[6][7]= {
-                  {0,0,0,0,0,0,0},
-                  {0,0,0,0,0,0,0},
-                  {0,0,0,0,0,0,0},
-                  {0,0,0,0,0,0,0},
-                  {0,0,0,0,0,0,0},
-                  {0,0,0,0,0,0,0}
-                  };
-//requires: nothing 
-//effects: prints the board of 6 rows and 7 columns 
-void printBoard(){
+
+//requires: a 2D board of 6 rows and 7 columns 
+//effects: prints the board
+void printBoard(int board[6][7]){
     printf("- - - - - - - - - - - - - - -\n");
     for(int row= 0; row<6; row++){
         for(int col=0; col<7; col++){
@@ -41,7 +34,7 @@ int fairCoin(){
 }
 //requires: nothing
 //effects: returns the name of the player having the red color
-//if the value returned by fairCoin() is [0-4], player1 will be red. Otherwise, player2 will take thwe red color.
+//if the value returned by fairCoin() is [0-4], player1 will be red. Otherwise, player2 will take the red color.
 char* pickPlayer(){
     int randomNumber = fairCoin();
     if (0 <= randomNumber && randomNumber <= 4){
@@ -53,7 +46,7 @@ char* pickPlayer(){
 }
 //requires: takes the updated board
 //effects: returns 1 if there is a horizontal line of 4 consecutive pieces having the same color; otherwise, returns 0.
-int checkHorizentally(){
+int checkHorizentally(int board[6][7]){
     for (int row=5; row>=0; row--){ 
         for (int column=3; column>=0; column--){ 
             if(board[row][column] != 0 && board[row][column] == board[row][column+1] && board[row][column] == board[row][column+2] && board[row][column] == board[row][column+3]){
@@ -63,9 +56,9 @@ int checkHorizentally(){
     return 0;
 }
 }
-//requires: nothing
+//requires: takes the updated board
 //effects: returns 1 if there is a vertical line of 4 consecutive pieces having the same color; otherwise, returns 0.
-int checkVertically(){
+int checkVertically(int board[6][7]){
     for (int column=0; column<=6; column++){
         for (int row=2; row>=0; row--){
             if(board[row][column] != 0 && board[row][column] == board[row+1][column] && board[row][column] == board[row+2][column] && board[row][column] == board[row+3][column]){
@@ -75,9 +68,9 @@ int checkVertically(){
     return 0;
 }
 }
-//requires: nothing
+//requires: takes the updated board
 //effects: returns 1 if there is a diagonal line of 4 consecutive pieces having the same color; otherwise, returns 0.
-int checkDiagonally(){
+int checkDiagonally(int board[6][7]){
     for(int row=0;row<=2;row++){
         for(int column=0; column<=3;column++ ){
             if(board[row][column] != 0 && board[row][column]==board[row+1][column+1] && board[row][column] ==board[row+2][column+2] && board[row][column]==board[row+3][column+3]){
@@ -96,28 +89,28 @@ int checkDiagonally(){
     return 0;
 }
 
-//requires: A +ve int int the range of [0-6] representing the column selectd by the player 
+//requires: A 2D board of 6 rows and 7 columns and a +ve int in the range of [0-6] representing the column selectd by the player 
 //effects: returns 1 if the corresponding column is full, and 0 otherwise. 
-int checkColumnFull(int col){
+int checkColumnFull(int board[6][7] ,int col){
     if (board[0][col]){
         return 1;
     }
     return 0;
-}
+} 
 //requires: The updated board
 //effects: returns 1 if the board is full; 0 otherwise.
-int isBoardFull(){
+int isBoardFull(int board[6][7]){
     int counter = 0;
     for(int column=0; column <= 6; column++){
-        if(checkColumnFull(column)) counter++; }
+        if(checkColumnFull(board,column)) counter++; }
     if(counter==7) return 1;
     return 0;
 }
 
-//requires: a +ve int in the range [0-6] representing the intended column by the user.
-//effects: returns the number of the row corresponding to the first empty cell (for bottom to top)
+//requires: a 2D board of 6 rows and 7 columns and a +ve int in the range [0-6] representing the selected column by the user.
+//effects: returns the number of the row corresponding to the first empty cell (from bottom to top)
 //         of the entered column; -1 if the column is full.
-int getFirstEmptyRow(int column){
+int getFirstEmptyRow(int board[6][7], int column){
     for(int row = 5; row >= 0; row--){
         if(board[row][column] == 0){
             return row;
@@ -125,33 +118,39 @@ int getFirstEmptyRow(int column){
     }
     return -1;
 }
-//requires: a +ve int in the range [0-6] corresponding to the column entered by the user. And a +ve int [1-2]
+//requires: a 2D board of 6 rows and 7 columns and a +ve int in the range [0-6] corresponding to the column entered by the user. And a +ve int [1-2]
 //          corresponding to the color of the player.
-//effects: updates the board based on the player's choice. 
-void updateBoard(int column, int color){
-    if(!checkColumnFull(column)){
-        int row = getFirstEmptyRow(column);
+//effects: returns 1 after a successful update of the board ( intended column is not full), 0 otherwise. 
+int updateBoard(int board[6][7],int column, int color){
+    if(!checkColumnFull(board, column)){
+        int row = getFirstEmptyRow(board, column);
         board[row][column] = color;
-        return;
+        return 1;
+        
     }
-    else if (column < 6){
-        updateBoard(column+1, color);
+    return 0;
 
-    }else{
-        updateBoard(0, color);
-    }
     
 }
-//requires: nothing 
+//requires: a 2D board of 6 rows and 7 columns 
 //effects: returns 1 if the player connected 4 entries successfully, 0 otherwise 
-int winningMove(){
-    if(checkHorizentally() || checkVertically() || checkDiagonally()){
+int winningMove(int board[6][7]){
+    if(checkHorizentally(board) || checkVertically(board) || checkDiagonally(board)){
         return 1;
     }else{
         return 0;
     }
 }
 int main(){
+    int board[6][7]= {
+                  {0,0,0,0,0,0,0},
+                  {0,0,0,0,0,0,0},
+                  {0,0,0,0,0,0,0},
+                  {0,0,0,0,0,0,0},
+                  {0,0,0,0,0,0,0},
+                  {0,0,0,0,0,0,0}
+                  };
+
     printf("player 1: Enter your name (single word, no spaces): ");
     scanf("%s",player1);
     printf("player 2: Enter your name (single word, no spaces): ");
@@ -160,7 +159,7 @@ int main(){
     char* playerHavingRed = pickPlayer();
     char* playerHavingYellow = (strcmp(playerHavingRed,player1) == 0) ? player2 : player1;
     printf("%s you'll take the red color, \n%s you'll take the yellow color\n",playerHavingRed, playerHavingYellow);
-    printBoard();
+    printBoard(board);
 
     while(!isBoardFull(board)){
         printf("\n");
@@ -168,28 +167,48 @@ int main(){
         start = clock();
         scanf("%d", &entry);
         end = clock();
-        updateBoard(entry, 1);
-        printBoard();
-        if(winningMove()){
+        int isBoardUpdated1= updateBoard(board,entry, 1);
+        while(!isBoardUpdated1){
+            printf("\nThe selected column is full!\n");
+            printBoard(board);
+            printf("Enter another valid column number: ");
+            start=clock();
+            scanf("%d", &entry);
+            end = clock();
+            timeTakenByPlayerHavingRed+=(long double)(end-start)/CLOCKS_PER_SEC;
+            isBoardUpdated1=updateBoard(board,entry,1);
+        }
+        printBoard(board);
+        if(winningMove(board)){
             strcpy(winner, playerHavingRed);
             printf("Congrats %s, you\'re the winner!",playerHavingRed);
             break;
         }
-        timeTakenByPlayerHavingRed += (double)(end-start)/CLOCKS_PER_SEC;
+        timeTakenByPlayerHavingRed += (long double)(end-start)/CLOCKS_PER_SEC;
         printf("%s select a column number (between 0 and 6): ", playerHavingYellow);
         start = clock();
         scanf("%d", &entry);
         end = clock();
-        updateBoard(entry, 2);
-        printBoard();
-        if(winningMove()){
+        int isBoardUpdated2= updateBoard(board,entry, 2);
+        while(!isBoardUpdated2){
+            printf("\nThe selected column is full!\n");
+            printBoard(board);
+            printf("Enter another valid column number: ");
+            start=clock();
+            scanf("%d", &entry);
+            end = clock();
+            timeTakenByPlayerHavingYellow+=(long double)(end-start)/CLOCKS_PER_SEC;
+            isBoardUpdated2=updateBoard(board,entry,2);
+        }
+        printBoard(board);
+        if(winningMove(board)){
             strcpy(winner, playerHavingYellow);
             printf("Congrats %s, you\'re the winner!",playerHavingYellow);
             break;
         }
-        timeTakenByPlayerHavingYellow += (double)(end-start)/CLOCKS_PER_SEC;
+        timeTakenByPlayerHavingYellow += (long double)(end-start)/CLOCKS_PER_SEC;
     }
-    if(isBoardFull()){
+    if(isBoardFull(board)){
         if(timeTakenByPlayerHavingRed < timeTakenByPlayerHavingYellow){
             printf("Time taken by %s = %d seconds.\n",playerHavingRed,timeTakenByPlayerHavingRed);
             printf("Time taken by %s = %d seconds.\n",playerHavingYellow,timeTakenByPlayerHavingYellow);
